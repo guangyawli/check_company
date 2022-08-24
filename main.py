2f90b88ee5fd9242f8961eb45ba9ac07bbd617a2
guangyaw <guangyaw@hotmail.com>
2022-08-24 22:19:08 +0800

    修改為持續新增相關的公司資料，不再新增檔案

--- main.py
+++ main.py
@@ -11,13 +11,40 @@ import openpyxl
 import datetime
 from os.path import exists
 
-FilePath = (str(datetime.date.today()) + "-" + "company.xlsx")
+FilePath = ("final_company.xlsx")
+final_column_len = 0
+
+
+def write_more_info(old_names,name_list, address_list, others_info):
+    # 讀取 Excel 檔案
+    wb = openpyxl.load_workbook(FilePath)
+    sheet = wb.active
+    final_column_len = len(old_names)
+
+    x = 0
+    if final_column_len > 0:
+        #print(final_column_len)
+        l = len(others_info)
+        #print(final_column_len + l)
+        for j in range(final_column_len+1, final_column_len+l):
+            sheet.cell(row=j, column=1).value = name_list[x]
+            sheet.cell(row=j, column=2).value = address_list[x]
+            sheet.cell(row=j, column=3).value = others_info[x][1]
+            sheet.cell(row=j, column=4).value = others_info[x][3]
+            sheet.cell(row=j, column=5).value = others_info[x][5]
+            sheet.cell(row=j, column=6).value = others_info[x][7]
+            sheet.cell(row=j, column=7).value = others_info[x][9]
+            sheet.cell(row=j, column=8).value = others_info[x][14]
+            x = x + 1
+    # print(others_info[0])
+    wb.save(FilePath)
+    wb.close()
 
 
 def write_others_info(others_info):
     # 讀取 Excel 檔案
     wb = openpyxl.load_workbook(FilePath)
-    sheet = wb['Sheet1']
+    sheet = wb.active
     # 以行號、列號指定儲存格
     sheet.cell(row=1, column=1).value = "公司名稱"
     sheet.cell(row=1, column=2).value = "地址"
@@ -28,13 +55,13 @@ def write_others_info(others_info):
     sheet.cell(row=1, column=7).value = others_info[0][8]
     sheet.cell(row=1, column=8).value = others_info[0][13]
 
-    for j in range(2,len(others_info)+2):
-        sheet.cell(row=j, column=3).value = others_info[j-2][1]
-        sheet.cell(row=j, column=4).value = others_info[j-2][3]
-        sheet.cell(row=j, column=5).value = others_info[j-2][5]
-        sheet.cell(row=j, column=6).value = others_info[j-2][7]
-        sheet.cell(row=j, column=7).value = others_info[j-2][9]
-        sheet.cell(row=j, column=8).value = others_info[j-2][14]
+    for j in range(2, len(others_info) + 2):
+        sheet.cell(row=j, column=3).value = others_info[j - 2][1]
+        sheet.cell(row=j, column=4).value = others_info[j - 2][3]
+        sheet.cell(row=j, column=5).value = others_info[j - 2][5]
+        sheet.cell(row=j, column=6).value = others_info[j - 2][7]
+        sheet.cell(row=j, column=7).value = others_info[j - 2][9]
+        sheet.cell(row=j, column=8).value = others_info[j - 2][14]
 
     #print(others_info[0])
     wb.save(FilePath)
@@ -57,8 +84,8 @@ def get_company_info(xurl):
         if "公司網址" and "產業類別" in objstag[i].text:
 
             if len(objstag[i].text) > 80:
-                #print("=========================== start   ========================at" + str(i))
-                #print(objstag[i].text)
+                # print("=========================== start   ========================at" + str(i))
+                # print(objstag[i].text)
                 objstag[i].text  # .split("產業類別")[1].split("產業描述")[0]
                 tmp_str.append(objstag[i].text)
 
@@ -67,11 +94,20 @@ def get_company_info(xurl):
 
 def print_hi():
     file_exists = exists(FilePath)
-
+    max_col = 0
+    old_datas = []
     if file_exists is True:
         ExcelWorkbook = openpyxl.load_workbook(FilePath)
         writer = pandas.ExcelWriter(FilePath, engine='openpyxl')
         writer.book = ExcelWorkbook
+
+        ws = ExcelWorkbook.active
+        first_column = ws['A']
+        final_column_len = len(first_column)
+        # Print the contents
+        for x in range(len(first_column)):
+            old_datas.append(first_column[x].value)
+        # print(old_datas)
 
     tmp_name_target = []
     tmp_address = []
@@ -96,22 +132,26 @@ def print_hi():
 
             for i in range(5, len(objstag)):
                 if objstag[i].text.rstrip() not in tmp_name_target:
-                    tmp_name_target.append(objstag[i].text.rstrip())
-                    tmp_address.append(str(objstag[i]).split("公司住址：")[1].split('">')[0])
-                    tmp_others=get_company_info("https:" + str(objstag[i]).split('" target=')[0].split('href="')[1])
-                    tmp_others_list.append(tmp_others[0].split("  "))
+                    if objstag[i].text.rstrip() not in old_datas:
+                        tmp_name_target.append(objstag[i].text.rstrip())
+                        tmp_address.append(str(objstag[i]).split("公司住址：")[1].split('">')[0])
+                        tmp_others = get_company_info(
+                            "https:" + str(objstag[i]).split('" target=')[0].split('href="')[1])
+                        tmp_others_list.append(tmp_others[0].split("  "))
 
-    final_target = pandas.Series(tmp_address, index=tmp_name_target)
     if file_exists is True:
-        # final_target.to_excel(writer, sheet_name=str(datetime.date.today()), header=False, startcol=0, startrow=1)
-        final_target.to_excel(writer, header=False, startcol=0, startrow=1, sheet_name='Sheet1')
-
-        writer.save()
         writer.close()
-    else:
-        final_target.to_excel(FilePath, header=False, startcol=0, startrow=1, sheet_name='Sheet1')
+        final_column_len = len(tmp_name_target)
+        #print(final_column_len)
+        if final_column_len > 0:
+            print("new_company："+ str(final_column_len-1))
+            write_more_info(old_datas, tmp_name_target, tmp_address,  tmp_others_list)
 
-    write_others_info(tmp_others_list)
+
+    else:
+        final_target = pandas.Series(tmp_address, index=tmp_name_target)
+        final_target.to_excel(FilePath, header=False, startcol=0, startrow=1, sheet_name='Sheet1')
+        write_others_info(tmp_others_list)
 
 
 # Press the green button in the gutter to run the script.
